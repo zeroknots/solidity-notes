fa13383188af441fdda5e1508dc98467046f7c35
zeroknots <zeroknots@protonmail.com>
2023-03-21 10:53:27 +0700

    cleaned up

--- script/MyEIP712.sol
+++ script/MyEIP712.sol
@@ -4,12 +4,10 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import "forge-std/console2.sol";
 
-
 import "openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
 import "openzeppelin-contracts/contracts/utils/cryptography/draft-EIP712.sol";
 
 contract MyEIP712 is EIP712 {
-
     struct MyData {
         address owner;
         uint256 myParam;
@@ -20,46 +18,47 @@ contract MyEIP712 is EIP712 {
     mapping(address => MyData) public userToData;
     mapping(address => uint256) public nonces;
 
-    constructor() EIP712("MyEIP712", "4"){
+    constructor() EIP712("MyEIP712", "4") {}
 
+    function digest(address owner, uint256 myParam, uint256 deadline) public view returns (bytes32) {
+        return _hashTypedDataV4(
+            keccak256(
+                abi.encode(
+                    keccak256("MyData(address owner,uint256 myParam,uint256 nonce,uint256 deadline)"),
+                    owner,
+                    myParam,
+                    nonces[owner],
+                    deadline
+                )
+            )
+        );
     }
 
-    function digest(address owner, uint256 myParam, uint256 deadline) public returns(bytes32){
-
-        return _hashTypedDataV4(keccak256(abi.encode(
-            keccak256("MyData(address owner,uint256 myParam,uint256 nonce,uint256 deadline)"),
-            owner,
-            myParam,
-            nonces[owner],
-            deadline
-        )));
-
-    }
-    function executeMyFunctionFromSignature(
-        bytes memory signature,
-        address owner,
-        uint256 myParam,
-        uint256 deadline
-    ) external {
+    function executeMyFunctionFromSignature(bytes memory signature, address owner, uint256 myParam, uint256 deadline)
+        external
+    {
         bytes32 digest = digest(owner, myParam, deadline);
         address signer = ECDSA.recover(digest, signature);
-        require(signer == owner, "MyData: invalid signature");
-        require(signer != address(0), "ECDSA: invalid signature");
-        require(block.timestamp < deadline, "Mydata: signed transaction expired");
+        if (block.timestamp > deadline) revert ExpiredSignature();
+        if (signer == address(0)) revert AddressZero();
+        if (signer != owner) revert InvalidSignature();
         console2.log("MyData: signature verified");
 
         userToData[owner] = MyData(signer, myParam, nonces[owner], deadline);
 
         nonces[owner]++;
     }
+
+    error InvalidSignature();
+    error ExpiredSignature();
+    error AddressZero();
 }
 
-contract NotesEIP712 is Script {
-
+contract DemoEIP712 is Script {
     MyEIP712 myEIP712;
+
     function setUp() public {
         myEIP712 = new MyEIP712();
-
     }
 
     function run() public {
